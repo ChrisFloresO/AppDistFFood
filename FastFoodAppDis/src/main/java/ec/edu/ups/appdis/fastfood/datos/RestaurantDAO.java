0bf6e0db6a818636c44201dac32a0b8bede805c5
Christian <cris-fernan17@hotmail.com>
2017-12-04 03:54:50 -0500

    Restaurante

--- FastFoodAppDis/src/main/java/ec/edu/ups/appdis/fastfood/datos/RestaurantDAO.java
+++ FastFoodAppDis/src/main/java/ec/edu/ups/appdis/fastfood/datos/RestaurantDAO.java
@@ -1,11 +1,15 @@
 package ec.edu.ups.appdis.fastfood.datos;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import ec.edu.ups.appdis.fastfood.modelo.Restaurante;
 import ec.edu.ups.appdis.fastfood.modelo.Usuario;
+
 @Stateless
 public class RestaurantDAO {
 	
@@ -18,12 +22,21 @@ public class RestaurantDAO {
 		public void actualizar(Restaurante r) {
 			em.merge(r);
 		}
-		public void borrar(String  nombre) {
-			em.remove(leer(nombre));
+		public void borrar(int codigo) {
+			em.remove(leer(codigo));
 		}
-		public Restaurante leer(String  nombre) {
-			em.find(Restaurante.class, nombre);
+		public Restaurante leer(int  codigo) {
+			em.find(Restaurante.class, codigo);
 			return null;
 		}	
+		public List<Restaurante> listadoRestaurantes()
+		{
+			//selects contra las entidades mapeadas
+			String jppql = "SELECT r FROM Restaurante r";
+			Query query = em.createQuery(jppql,Restaurante.class);
+			@SuppressWarnings("unchecked")
+			List<Restaurante> listado =query.getResultList();
+			return listado;
+		}
 
 }
